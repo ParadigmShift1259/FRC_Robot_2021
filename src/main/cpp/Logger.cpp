6cad63fe74aefdc87d9866112ac54939f415a0a1
scottcoursin <scott.coursin@gmail.com>
2021-01-22 16:30:08 -0600

    Updated logger to match python code

--- src/main/cpp/Logger.cpp
+++ src/main/cpp/Logger.cpp
@@ -9,6 +9,8 @@
 
 #include "Logger.h"
 
+#include <dirent.h>
+
 const std::vector<char> c_vecLevels
 {
       'D'
@@ -17,9 +19,8 @@ const std::vector<char> c_vecLevels
     , 'E'
 };
 
-Logger::Logger(const char *path, bool console_echo)
+Logger::Logger(bool console_echo)
     : m_fd(nullptr)
-    , m_path(path)
     , m_console_echo(console_echo)
 {
     m_formattedIntData.reserve(500);
@@ -28,20 +29,21 @@ Logger::Logger(const char *path, bool console_echo)
 
 void Logger::openLog()
 {
-    printf("Opening log %s\n", m_path.c_str());
+    std::string path = c_logFileBasePath + FindNextLogFileNumber() + c_logFileExtension;
+    printf("Opening log %s\n", path.c_str());
     if (m_fd != nullptr)
     {
         printf("Log was open, closing\n");
         closeLog();
     }
 
-    m_fd = fopen(m_path.c_str(), "a");
+    m_fd = fopen(path.c_str(), "a");
     // CSV header for free form text logging
     if (m_fd != nullptr)
-        fprintf(m_fd, "Timestamp,Level,Function,Line,Message\n");
+        fprintf(m_fd, "Timestamp,Header,Source,Level,Message\n");
 
     if (m_console_echo)
-        printf("Timestamp,Level,Function,Line,Message\n");
+        printf("Timestamp,Header,Source,Level,Message\n");
 }
 
 Logger::~Logger()
@@ -63,7 +65,36 @@ void Logger::closeLog()
     }
 }
 
-void Logger::logMsg(ELogLevel level, const char* func, const int line, const char* msg, const char* msg2 /* = nullptr */)
+std::string Logger::FindNextLogFileNumber()
+{
+    int nextLogFileNumber = 0;
+    struct dirent *dir;
+    DIR* d = opendir("/tmp");
+    //printf("Existing log files\n");
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            //printf("%s\n", dir->d_name);
+            std::string filename(dir->d_name);
+            auto pos = filename.find("logfile");
+            if (pos != std::string::npos)
+            {
+                //printf("filename.substr(pos = %d, 3) %s\n", pos, filename.substr(pos + 7, 3).c_str());
+                nextLogFileNumber = std::max(nextLogFileNumber, std::stoi(filename.substr(pos + 7, 3)));
+            }
+        }
+
+        nextLogFileNumber++;
+        closedir(d);
+    }
+
+    char buf[4];
+    sprintf(buf, "%03d", nextLogFileNumber);
+    return buf;
+}
+
+void Logger::logMsg(ELogLevel level, const char* source, const char* msg, const char* msg2 /* = nullptr */, bool bHeader /* = c_bData */)
 {
     if (m_fd == nullptr)
     {
@@ -75,63 +106,24 @@ void Logger::logMsg(ELogLevel level, const char* func, const int line, const cha
         float timestamp = m_timer.GetFPGATimestamp();
         if (msg2 == nullptr)
         {
-	        fprintf(m_fd, "%.6f,%c,%s,%d,%s\n", timestamp, c_vecLevels[level], func, line, msg);
+	        fprintf(m_fd, "%.6f,%d,%s,%c,%s\n", timestamp, bHeader ? 1 : 0, source, c_vecLevels[level], msg);
         }
         else
         {
-            fprintf(m_fd, "%.6f,%c,%s,%d,%s,%s\n", timestamp, c_vecLevels[level], func, line, msg, msg2);
+            fprintf(m_fd, "%.6f,%d,%s,%c,%s,%s\n", timestamp, bHeader ? 1 : 0, source, c_vecLevels[level], msg, msg2);
         }
         
         if (m_console_echo)
         {
             if (msg2 == nullptr)
             {
-	            printf("%.6f,%c,%s,%d,%s\n", timestamp, c_vecLevels[level], func, line, msg);
+                printf("%.6f,%d,%s,%c,%s\n", timestamp, bHeader ? 1 : 0, source, c_vecLevels[level], msg);
             }
             else
             {
-                printf("%.6f,%c,%s,%d,%s,%s\n", timestamp, c_vecLevels[level], func, line, msg, msg2);
+                printf("%.6f,%d,%s,%c,%s,%s\n", timestamp, bHeader ? 1 : 0, source, c_vecLevels[level], msg, msg2);
             }
         }
-    }
-}
-
-void Logger::logData(const char* func, const int line, const vector<double*>& data)
-{
-    formatData(data);
-    logMsg(eInfo, func, line, m_formattedDoubleData.c_str());
-}
-
-void Logger::logData(const char* func, const int line, const vector<int*>& data)
-{
-    formatData(data);
-    logMsg(eInfo, func, line, m_formattedIntData.c_str());
-}
-
-void Logger::logData(const char* func, const int line, const vector<int*>& dataInt, const vector<double*>& dataDouble)
-{
-    formatData(dataInt);
-    formatData(dataDouble);
-    logMsg(eInfo, func, line, m_formattedIntData.c_str(), m_formattedDoubleData.c_str());
-}
-
-void Logger::formatData(const vector<double*>& data)
-{
-	//									   123456789
-    constexpr int c_charsPerDouble = 9;	// -123.000, most will be small but allow for some to be larger
-    const size_t c_numDataPts = data.size();
-    const int c_totalSz = c_charsPerDouble * c_numDataPts - 1;  // -1 to truncate the terminating null
-    m_formattedDoubleData.resize(c_totalSz);
-    char* out = const_cast<char*>(m_formattedDoubleData.c_str());
-
-    for (size_t i = 0; i < c_numDataPts; i++)
-    {
-        //out += sprintf(out, "%.3f%s", *data[i], i == c_numDataPts - 1 ? "" : ",");
-        out += sprintf(out, "%.3f%c", *data[i], i < c_numDataPts - 1 ? ',' : ' ');
- 		if (out - m_formattedDoubleData.c_str() >= c_totalSz)
-		{
-			break;
-		}
     }
 }
 
@@ -146,32 +138,11 @@ void Logger::formatData(const vector<double>& data)
 
     for (size_t i = 0; i < c_numDataPts; i++)
     {
-        //out += sprintf(out, "%.3f%s", data[i], i == c_numDataPts - 1 ? "" : ",");
         out += sprintf(out, "%.3f%c", data[i], i < c_numDataPts - 1 ? ',' : ' ');
  		if (out - m_formattedDoubleData.c_str() >= c_totalSz)
 		{
 			break;
 		}
-    }
-}
-
-void Logger::formatData(const vector<int*>& data)
-{
-    //									   1234567
-    constexpr int c_charsPerInt = 7;	// _ _ _0,
-    const size_t c_numDataPts = data.size();
-    const int c_totalSz = c_charsPerInt * c_numDataPts - 1;  // -1 to truncate the terminating null
-    m_formattedIntData.resize(c_totalSz);
-    char* out = const_cast<char*>(m_formattedIntData.c_str());
-
-    for (size_t i = 0; i < c_numDataPts; i++)
-    {
-        //out += sprintf(out, "%6d%s", *data[i], i == c_numDataPts - 1 ? "" : ",");
-        out += sprintf(out, "%6d%c", *data[i], i < c_numDataPts - 1 ? ',' : ' ');
-        if (out - m_formattedIntData.c_str() >= c_totalSz)
-        {
-            break;
-        }
     }
 }
 
@@ -186,7 +157,6 @@ void Logger::formatData(const vector<int>& data)
 
     for (size_t i = 0; i < c_numDataPts; i++)
     {
-        //out += sprintf(out, "%6d%s", data[i], i == c_numDataPts - 1 ? "" : ",");
         out += sprintf(out, "%6d%c", data[i], i < c_numDataPts - 1 ? ',' : ' ');
         if (out - m_formattedIntData.c_str() >= c_totalSz)
         {
